1c8fb10e0b7ca34b6f70af627649dce2fda5dad3
chandu-Narmeta <chandunarmeta8@gmail.com>
2024-12-02 15:19:59 +0530

    need to change

--- src/component/Child.jsx
+++ src/component/Child.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import './comp.css'
 
-function Child(prop,chref) {
+function Child(props,chref) {
+    console.log(props.addname)
   return (
-    <div class="child">
+    <div className="child">
         <h1>child component</h1>
         <input type="text" ref={chref} />
+         {
+          props.addname?.map(function(e){
+              return <li>{e}</li>
+          })
+         }
     </div>
   )
 }
--- src/component/Parent.jsx
+++ src/component/Parent.jsx
@@ -1,15 +1,22 @@
-import React, { useCallback, useEffect, useRef } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import Child from './Child'
 import './comp.css'
 
 function Parent() {
+var [name,setName] = useState()
 var pref1 = useRef()
 var pref2 = useRef()
 var cref = useRef()
 
+ var addname = useCallback( function (){
+   var temp = pref1.current.value
+   var temp2 = pref2.current.value
+   setName([temp,temp2])  
+ }) 
+
 useEffect(()=>{
     pref1.current.focus()
-},[])
+},[name])
 
 function changeref(event){
     if(event.key==='Enter'){
@@ -25,12 +32,13 @@ var changechildref = useCallback(
 )
 
   return (
-    <div class="parent">
+    <div className="parent">
       <input type="text" ref={pref1} onKeyUp={(e)=>{changeref(e)}} />
       <br />
       <input type="text" ref={pref2} onKeyUp={(e)=>{changechildref(e)}} />
+       <button onClick={()=>{addname()}}>add name</button>
       <div>
-        <Child ref={cref}/>
+        <Child ref={cref} parname={addname} />
       </div>
     </div>
   )
